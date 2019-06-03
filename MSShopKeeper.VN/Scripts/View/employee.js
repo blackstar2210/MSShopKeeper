8fbf9385b13b8b3e95fb3e4401258ebc7bd5f9bb
cchan <cchan@FRESHER-50>
2019-06-03 17:34:22 +0700

    Add Dialog

--- MSShopKeeper.VN/Scripts/View/employee.js
+++ MSShopKeeper.VN/Scripts/View/employee.js
@@ -1,48 +1,115 @@
-﻿$(document).ready(function () {
+﻿//showFilter1 = 0;
+//showFilter2 = 0;
+//showFilter3 = 0;
+//showFilter4 = 0;
+//showFilter5 = 0;
+//showFilter6 = 0;
+showFilter = 0;
+$(document).ready(function () {
     //load dữ liệu
     //employeeJs.loadData();
-    employeeJs = new employeeJs();
+    employeeJs.initEvents();
 
+    //Khởi tạo dialog
     dialog = $('.dialogEmployeeDetail').dialog({
-        width: 600,
-        height: 200,
+        width: 820,
+        height: 760,
         autoOpen: false,
         closeOnEscape: false,
         modal: true,
-        buttons: [
-            {
-                text: 'Thêm',
-                id: "btnAdd",
-                width: 50,
-                height: 25,
-                manhnv: 'sdfjklashdfjiasdfhjkasfhdk',
-                click: function () {
-                    alert('hello');
-                }
-            },
-            {
-
-                width: 50,
-                height: 25,
-                text: 'Sửa',
-                id: 'btnEdit'
-            },
-            {
-                text: 'Đóng',
-                width: 60,
-                height: 25,
-                click: function () {
-                    dialog.dialog('close');
-                }
-            },
-        ]
     })
+
     // Gán Events:
     $('#btnAddLayout').click(function () {
         dialog.dialog('open');
-    })
+    });
+
+    //$("#button-filter1").click(function () {
+    //    if (showFilter1 === 0) {
+    //        $('#dropdown-filter1').show();
+    //        showFilter1 = 1;
+    //    } else {
+    //        $('#dropdown-filter1').hide();
+    //        showFilter1 = 0;
+    //    }
+    //});
+
+    //$("#button-filter2").click(function () {
+    //    if (showFilter2 === 0) {
+    //        $('#dropdown-filter2').show();
+    //        showFilter2 = 1;
+    //    } else {
+    //        $('#dropdown-filter2').hide();
+    //        showFilter2 = 0;
+    //    }
+    //});
+
+    //$("#button-filter3").click(function () {
+    //    if (showFilter3 === 0) {
+    //        $('#dropdown-filter3').show();
+    //        showFilter3 = 1;
+    //    } else {
+    //        $('#dropdown-filter3').hide();
+    //        showFilter3 = 0;
+    //    }
+    //});
+
+    //$("#button-filter4").click(function () {
+    //    if (showFilter4 === 0) {
+    //        $('#dropdown-filter4').show();
+    //        showFilter4 = 1;
+    //    } else {
+    //        $('#dropdown-filter4').hide();
+    //        showFilter4 = 0;
+    //    }
+    //});
+
+    //$("#button-filter5").click(function () {
+    //    debugger
+    //    if (showFilter5 === 0) {
+    //        $('#dropdown-filter5').show();
+    //        showFilter5 = 1;
+    //        debugger
+    //    } else {
+    //        $('#dropdown-filter5').hide();
+    //        showFilter5 = 0;
+    //    }
+    //});
 });
 
+$(document).on("click", function (event) {
+    //var $trigger = $("#button-filter1");
+    //if ($trigger !== event.target && !$trigger.has(event.target).length) {
+    //    $("#dropdown-filter1").hide();
+    //    showFilter1 = 0;
+    //}
+
+    //var $trigger1 = $("#button-filter2");
+    //if ($trigger1 !== event.target && !$trigger1.has(event.target).length) {
+    //    $("#dropdown-filter2").hide();
+    //    showFilter2 = 0;
+    //}
+
+    //var $trigger2 = $("#button-filter3");
+    //if ($trigger2 !== event.target && !$trigger2.has(event.target).length) {
+    //    $("#dropdown-filter3").hide();
+    //    showFilter3 = 0;
+    //}
+
+    //var $trigger3 = $("#button-filter4");
+    //if ($trigger3 !== event.target && !$trigger3.has(event.target).length) {
+    //    $("#dropdown-filter4").hide();
+    //    showFilter4 = 0;
+    //}
+
+    //var $trigger4 = $("#button-filter5");
+    //if ($trigger4 !== event.target && !$trigger4.has(event.target).length) {
+    //    $("#dropdown-filter5").hide();
+    //    showFilter5 = 0;
+    //}
+});
+
+//Load dữ liệu mẫu với class Base
 class base {
     constructor() {
 
@@ -52,7 +119,7 @@ class base {
         var data = getData();
         var fiels = $("[fielname]");
         $.each(data, function (index, item) {
-            var rowHTML = $('<tr></tr>');
+            var rowHTML = $('<tr class="rowData"></tr>');
             $.each(fiels, function (index, fielItem) {
                 var fielName = fielItem.getAttribute('fielName');
                 if (fielName === "EmployeeCode") {
@@ -88,21 +155,100 @@ class base {
     }
 }
 
+//Tạo Employee kế thừa class Base
 class employeeJs extends base {
     constructor() {
         super();
         this.name = "Han";
         this.loadData();
     }
+
+    //Khởi tạo event cho Employee
+    initEvents() {
+        $('.button-filter').on('click', this.showFilterMenu);
+        $('.filter-item').on('click', this.changeCurrentFilter);
+        $('.rowData').on('click', this.changeBackground);
+        $(document).on('click', this.hideAllDropDown);
+    }
+
+    //Xử lý khi click vào button filter
+    showFilterMenu() {
+        var filterContainer = $(this).parents('.row-filter');
+        var currentMenu = filterContainer.find('.dropdown-filter');
+        $('.dropdown-filter').not(currentMenu).hide();
+        currentMenu.toggle();
+        //if (showFilter === 0) {
+            
+        //    showFilter = 1;
+        //}
+        //else {
+        //    currentMenu.hide();
+        //    showFilter = 0;
+        //}
+        
+    }
+
+    hideAllDropDown(event) {
+        var target = $(event.target);
+        if (target.parents('.row-filter').length === 0 || target.hasClass('text-filter') || target.hasClass('button-filter-date')) {
+            $('.dropdown-filter').hide();
+            // showFilter = 0;
+        }
+    }
+
+    //Thay đổi giá trị hiện tại của button filter
+    changeCurrentFilter() {
+        var filterContainer = $(this).parents('.dropdown-filter');
+        var rowFilter = filterContainer.parents('.row-filter');
+        var value = $(this).attr('data-filter');
+        var currentIcon = rowFilter.find('.current-filter');
+        currentIcon.html(value);
+        currentIcon.val(value);
+        filterContainer.hide();
+    }
+
+    //Xử lý click vào 1 hàng
+    changeBackground() {
+        $(this).css('background-color', 'rgb(195,236,255)');
+        $('.rowData').not($(this)).css('background-color', '');
+    }
+
 }
 
+//Datepicker khi click vào icon date
 $("#text-filter").datepicker({
+    inline: true,
+    showOtherMonths: true,
     showOn: "button"
 });
+
+$.datepicker.regional["vi-VN"] =
+    {
+        closeText: "Đóng",
+        prevText: "Trước",
+        nextText: "Sau",
+        currentText: "Hôm nay",
+        monthNames: ["Tháng một", "Tháng hai", "Tháng ba", "Tháng tư", "Tháng năm", "Tháng sáu", "Tháng bảy", "Tháng tám", "Tháng chín", "Tháng mười", "Tháng mười một", "Tháng mười hai"],
+        monthNamesShort: ["Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín", "Mười", "Mười một", "Mười hai"],
+        dayNames: ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"],
+        dayNamesShort: ["CN", "Hai", "Ba", "Tư", "Năm", "Sáu", "Bảy"],
+        dayNamesMin: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+        weekHeader: "Tuần",
+        dateFormat: "dd/mm/yy",
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ""
+    };
+
+$.datepicker.setDefaults($.datepicker.regional["vi-VN"]);
+
 
 //$("#valueFilter-input-datepicker").datepicker({
 //    showOn: "button"
 //});
+
+//Chuyển trạng thái click sang button trước đó
 $("#button-filter-date").on('click', function () {
     $(this).prev().trigger('click');
 });
@@ -126,6 +272,7 @@ $("#button-filter-date").on('click', function () {
 //    }
 //}
 
+//Tạo dữ liệu mẫu 
 function getData() {
     fakeData = [];
     for (var i = 0; i < 100; i++) {
@@ -143,6 +290,7 @@ function getData() {
     return fakeData;
 }
 
+//Xử lý dropdown trên header với JS
 function showDropdown() {
     document.getElementById("dropdown-selectbox").classList.toggle("show");
 }
@@ -175,6 +323,8 @@ window.onclick = function (event) {
         }
     }
 };
+
+employeeJs = new employeeJs();
 
 
 
